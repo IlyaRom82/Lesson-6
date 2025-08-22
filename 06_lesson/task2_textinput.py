03f49c559c6a369d9273514db8c6c451f7f5b8fb
IlyaRom82 <light108@tutamail.com>
2025-08-23 01:16:19 +0300

    Исправлены ошибки flake8 в Task 2 и Task 3

--- 06_lesson/task2_textinput.py
+++ 06_lesson/task2_textinput.py
@@ -29,4 +29,3 @@ try:
 
 finally:
     driver.quit()
-
--- 06_lesson/task3_image.py
+++ 06_lesson/task3_image.py
@@ -2,8 +2,9 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 driver.set_script_timeout(60)  # таймаут для async скриптов
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
-
+driver.get = (
+    ("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
+)
 try:
     script = """
     let callback = arguments[0];
@@ -11,7 +12,8 @@ try:
 
     function checkAllLoaded() {
         let imgs = Array.from(document.images);
-        if (imgs.length >= 4 && imgs.every(img => img.complete && img.naturalWidth > 0)) {
+        if (imgs.length >= 4 && imgs.every\
+        (img => img.complete && img.naturalWidth > 0)) {
             if (observer) observer.disconnect();  // отключаем observer
             callback(imgs.map(img => img.src));  // вызываем callback
         }
